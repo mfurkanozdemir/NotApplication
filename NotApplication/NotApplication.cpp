d6dc8c9295957e604ca94d6f8f38c77124964334
Muhammed Furkan Özdemir <mfurkanozdemir99@gmail.com>
2023-12-05 23:25:34 +0300

    dosyaKaydett fonksiyonu girildi

--- NotApplication/NotApplication.cpp
+++ NotApplication/NotApplication.cpp
@@ -45,38 +45,70 @@ void notlarıGörüntüle(const vector<Not> &notlar)
         
 }
 
-//void dosyayıKaydet(){}
-
-
-
+void dosyayıKaydet(const vector<Not> &notlar)
+{
+    ofstream dosya("ajanda.txt");
+    if (dosya.is_open())
+    {
+        for (const Not& note : notlar)
+        {
+            dosya << "Baslik: " << note.baslik << endl;
+            dosya << "Icerik: " << note.icerik << endl;
+            dosya << "Tarih: " << note.tarih << endl;
+            dosya << "-----------------------" << endl;
+        }
+        cout << "Notlar dosyaya kaydedildi" << endl;
+        dosya.close();
+    }
+    else
+    {
+        cout << "Dosya acilmadi." << endl;
+    }
+}
 
 int main()
 {
+    char yeniden;
     vector<Not> notlar;
 
     cout << "1. NOT EKLE" << endl;
     cout << "2. NOTLARI GORUNTULE" << endl;
     cout << "3. CIKIS" << endl;
+
     do
     {
-        int secim;
-        cout << "Seciminizi yapiniz: ";
-        cin >> secim;
-        cin.ignore();           // Bir önceki girişin sonlandırılması için
-
-
-        switch (secim)
+        cout << "Programa devam etmek ister misiniz? (E/H)" << endl;
+        cin >> yeniden;
+        if(yeniden=='E'|| yeniden == 'e')
         {
-        case 1:
-            notEkle(notlar);
-            break;
-        case 2:
-            notlarıGörüntüle(notlar);
-            break;
-        default:
-            cout << "Gecersiz secim. Tekrar deneyin." << endl;
-            break;
+            int secim;
+            cout << "Seciminizi yapiniz: ";
+            cin >> secim;
+            cin.ignore();           // Bir önceki girişin sonlandırılması için
+
+
+            switch (secim)
+            {
+            case 1:
+                notEkle(notlar);
+                break;
+            case 2:
+                notlarıGörüntüle(notlar);
+                break;
+            case 3:
+                dosyayıKaydet(notlar);
+                cout << "Program kapatiliyor" << endl;
+                break;
+            default:
+                cout << "Gecersiz secim. Tekrar deneyin." << endl;
+                break;
+            }
+        }
+        else
+        {
+            cout << "Program Kapatildi!";
+            return 0;
         }
     } while (true);
-    
+    return 0;
 }
